814faa789d33328613b532a2bd58bbe0dc4c53e1
brandybmw <brandybmw@yahoo.com>
2023-11-13 11:45:10 -0500

    Complete the test class for mirror

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,98 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+
+/*Given a string, look for a mirror image (backwards) string at both the beginning and end of the given string.
+In other words, zero or more characters at the very begining of the given string, and at the very end of the
+string in reverse order (possibly overlapping). For example, the string "abXYZba'' has the mirror end "ab''.
+
+Examples:
+
+mirrorEnds("abXYZba") ==> "ab"
+mirrorEnds("abca") ==> "a"
+mirrorEnds("aba") ==> "aba"
+A (buggy) implementation of this problem can be found in the Mirror class (mirror package). Apply everything you
+learned so far and find the bug. Write tests in the MirrorTest class. At the end, push your solution to GitHub.
+Tip: Think about even and uneven input lengths.
+BUG: mirrorEnds("abba") ==> "abba"
+*/
 public class MirrorTest {
+    //AAA: Arrange, Act, Assert
+    // indicates that the following method is a JUnit test
+    @Test
+    // The method name explains what we want to test.
+    public void mirrorTest1_oddMirrored() {
+    // body of test method invokes the function to test, passing something to it.
+        // ARRANGE & ACT
+        String result = new Mirror().mirrorEnds("abXYZba");
+
+        String expectedValue = "ab";
+
+        // ASSERT: assert that our result is equal to expected value
+        Assertions.assertEquals(expectedValue, result);
+    }
+    @Test
+    // The method name explains what we want to test.
+    public void mirrorTest2_evenMirrored() {
+        // body of test method invokes the function to test, passing something to it.
+        // ARRANGE & ACT
+        String result = new Mirror().mirrorEnds("abca");
+
+        String expectedValue = "a";
+
+        // ASSERT: assert that our result is equal to expected value
+        Assertions.assertEquals(expectedValue, result);
+    }
+    @Test
+    // The method name explains what we want to test.
+    public void mirrorTest3_evenAllMirrored() {
+        // body of test method invokes the function to test, passing something to it.
+        // ARRANGE & ACT
+        String result = new Mirror().mirrorEnds("aba");
+
+        String expectedValue = "aba";
+
+        // ASSERT: assert that our result is equal to expected value
+        Assertions.assertEquals(expectedValue, result);
+    }
+    @Test
+    // The method name explains what we want to test.
+    public void mirrorTest4_oddNoMirror() {
+        // body of test method invokes the function to test, passing something to it.
+        // ARRANGE & ACT
+        String result = new Mirror().mirrorEnds("abc");
+
+        String expectedValue = "";
+
+        // ASSERT: assert that our result is equal to expected value
+        Assertions.assertEquals(expectedValue, result);
+    }
+    @Test
+    // The method name explains what we want to test.
+    public void mirrorTest4_evenNoMirror() {
+        // body of test method invokes the function to test, passing something to it.
+        // ARRANGE & ACT
+        String result = new Mirror().mirrorEnds("abcd");
+
+        String expectedValue = "";
+
+        // ASSERT: assert that our result is equal to expected value
+        Assertions.assertEquals(expectedValue, result);
+    }
+
+    //FOUND THE BUG!!!!
+    @Test
+    // The method name explains what we want to test.
+    public void mirrorTest5_oddAllMirrored() {
+        // body of test method invokes the function to test, passing something to it.
+        // ARRANGE & ACT
+        String result = new Mirror().mirrorEnds("abba");
+
+        String expectedValue = "abba";
+
+        // ASSERT: assert that our result is equal to expected value
+        Assertions.assertEquals(expectedValue, result);
+    }
 }
--- src/test/java/tudelft/sum/TwoNumbersSumTest.java
+++ src/test/java/tudelft/sum/TwoNumbersSumTest.java
@@ -1,5 +1,76 @@
 package tudelft.sum;
 
-public class TwoNumbersSumTest {
+//added
+import java.util.ArrayList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+/*The function of addTwoNumbers should add two non-negative integers
+from which the digits are stored separately in an arraylist. For example,
+the number 12 would be stored as ArrayList(1,2). You can assume that
+there are no zeros in the beginning of the ArrayList i.e: not
+ArrayList(0, 0, 2);
+Input: l1 = [1,2,3], l2 = [6,5,4]
+Output: [7,7,7]
+Explanation: 123 + 654 = 777.
+*/
+
+/* Test template
+@Test
+public void testMethodName() {
+        int result = new ClassName().methodName(parameter);
+        Assertions.assertEquals(expected,result);
+        } */
+public class TwoNumbersSumTest {
+    @Test
+    public void twoDigit() {
+        // (ArrayList<Integer> first, ArrayList<Integer> second)
+        // Declaring the ArrayList with initial size n
+        // ArrayList<Integer> arr1 = new ArrayList<Integer>(n);
+        // arr1.add(1);
+        // arr1.add(2);
+        ArrayList<Integer> arr1 = new ArrayList<Integer>(2);
+        arr1.add(1);
+        arr1.add(2);
+        ArrayList<Integer> arr2 = new ArrayList<Integer>(2);
+        arr2.add(3);
+        arr2.add(4);
+        ArrayList<Integer> result = new TwoNumbersSum().addTwoNumbers(arr1,arr2);
+
+        ArrayList<Integer> expected = new ArrayList<Integer>(2);
+        expected.add (4);
+        expected.add (6);
+        Assertions.assertEquals(expected,result);
+    }
+
+    @Test
+    public void twoDigitCarry() {
+        ArrayList<Integer> arr1 = new ArrayList<Integer>(2);
+        arr1.add(7);
+        arr1.add(7);
+        ArrayList<Integer> arr2 = new ArrayList<Integer>(2);
+        arr2.add(1);
+        arr2.add(3);
+        ArrayList<Integer> result = new TwoNumbersSum().addTwoNumbers(arr1,arr2);
+
+        ArrayList<Integer> expected = new ArrayList<Integer>(2);
+        expected.add (9);
+        expected.add (0);
+        Assertions.assertEquals(expected,result);
+    }
+    @Test
+    public void zero() {
+        ArrayList<Integer> arr1 = new ArrayList<Integer>(2);
+        arr1.add(0);
+        arr1.add(0);
+        ArrayList<Integer> arr2 = new ArrayList<Integer>(2);
+        arr2.add(0);
+        arr2.add(0);
+        ArrayList<Integer> result = new TwoNumbersSum().addTwoNumbers(arr1, arr2);
+
+        ArrayList<Integer> expected = new ArrayList<Integer>(2);
+        expected.add(0);
+        expected.add(0);
+        Assertions.assertEquals(expected, result);
+    }
 }
